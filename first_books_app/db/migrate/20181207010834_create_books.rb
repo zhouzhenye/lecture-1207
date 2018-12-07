0064851612d44120c5b517cb0975984e0a79c5c8
zhouzhenye <zhouzhenye530@gmail.com>
2018-12-07 11:35:45 +0900

    12/7 課題提出

---
+++ first_books_app/db/migrate/20181207010834_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.text :memo
+      t.string :author
+      t.string :picture
+
+      t.timestamps
+    end
+  end
+end
